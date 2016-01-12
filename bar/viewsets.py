ad3735770d05554e9d1dbdf49d674c4acb805a79
Yannick Chabbert <yannick.chabbert@gmail.com>
2016-01-12 10:21:55 +0100

    make at least one valid app

--- bar/viewsets.py
+++ bar/viewsets.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Bar
+from .serializers import BarSerializer
 
 
 class BarViewSet(ModelViewSet):
     queryset = Bar.objects.all()
+    serializer_class = BarSerializer
